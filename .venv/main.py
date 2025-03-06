c7dac292cd8372ec6c8bf0d5c77ece405b0bdbb0
mateiporojan <porojanmatei@gmail.com>
2025-03-06 23:01:23 +0100

    dsa

---
+++ .venv/main.py
@@ -0,0 +1,13 @@
+import random
+import china
+import austria
+from latam.argentina import cook as argentina_cook
+from latam.brazil import cook as brazil_cook
+#from latam.mexico.yucatan import cook as yucatan_cook
+china.cook()
+china.greet()
+austria.cook()
+austria.greet()
+argentina_cook()
+brazil_cook()
+#yucatan_cook()
